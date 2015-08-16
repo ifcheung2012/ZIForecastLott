07cd084874a479b6b3a850e1e3dfe971eb9152af
if.cheung <if.cheung.2012@gmail.com>
2015-08-16 17:56:27 +0800

    1st commit

---
+++ ZIForecastLott/ZIForecastLott/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZIForecastLott
+//
+//  Created by IfCheung on 15/8/16.
+//  Copyright (c) 2015年 IfCheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
